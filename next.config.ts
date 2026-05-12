5e903d84ca4b163ec89f3a721318becb3eceb25b
ericlemiere <lemierecs@gmail.com>
2026-05-12 14:02:41 -0700

    added redirect

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  redirects: async () => {
+    return [
+      {
+        source: "/workout",
+        destination: "https://workout-app-pearl-kappa.vercel.app/",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
